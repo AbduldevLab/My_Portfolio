6a416305fb0a9734d28403a93e9afe120a884834
abdulhaouit <abderahman.haouit.2020@mumail.ie>
2024-04-26 20:52:34 +0100

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Footer(){
     return(
         <div class="text-center p-6 bg-black footer">
-        <span class="text-white">&copy; 2023 Copyright - </span>
-        <a class="link-underline text-white font-bold" href="abdul">A.Zino</a>
+        <span class="text-white">&copy; 2023 All rights reserved - </span>
+        <a class="link-underline text-white font-bold" href="abdul">Abdul@devLab.</a>
       </div>
     )
 }
